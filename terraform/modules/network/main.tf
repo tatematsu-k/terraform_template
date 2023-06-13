26aab85b29079578951bf1f531d31f1fadf47475
tatematsu-k <ynu1264@gmail.com>
2023-06-14 01:39:03 +0900

    temp

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -9,6 +9,14 @@ module "vpc" {
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  # 必要に応じて変更
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  # vpc flow log
+  enable_flow_log           = true
+  flow_log_destination_type = s3
+  flow_log_destination_arn  = var.flow_log_s3_arn
+  flow_log_file_format      = "parquet"
+  flow_log_traffic_type     = "ALL"
 }
